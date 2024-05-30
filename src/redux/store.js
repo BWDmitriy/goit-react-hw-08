6b90b38794a4927f007c3cfbebb627b66f5936cb
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-05-30 11:39:43 +0300

    init

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,19 +1,32 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // Використовуємо локальне сховище
 import contactsReducer from './contactsSlice';
 import filtersReducer from './filtersSlice';
 
+// Конфігурація для збереження поля items зі слайса контактів
+const contactsPersistConfig = {
+  key: 'contacts',
+  storage,
+  whitelist: ['items'], // Зберігаємо тільки поле items
+};
+
+// Застосовуємо конфігурацію до редюсера слайса контактів
+const persistedContactsReducer = persistReducer(contactsPersistConfig, contactsReducer);
 
 const store = configureStore({
   reducer: {
-    contacts: contactsReducer, 
+    contacts: persistedContactsReducer,
     filters: filtersReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
-        ignoredActions: [],
+        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
       },
     }),
 });
 
-export {store}
+const persistor = persistStore(store);
+
+export { store, persistor };
